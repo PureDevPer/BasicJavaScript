29683acf043089f1dd1346bd23143a1754627b67
PureDevPer <wooseok.kim1207@gmail.com>
2019-04-29 01:22:21 -0400

    Variable, Data Type

--- Variable/index.js
+++ Variable/index.js
@@ -4,3 +4,11 @@ a = 4;
 var c = 5;
 const d = 10;
 console.log(b, a, c, d);
+
+// -------------
+// Data type
+const str = "name";
+let boolTrue = true;
+let boolFalse = false;
+let fl = 12.34;
+console.log(str, boolTrue, boolFalse, fl);
