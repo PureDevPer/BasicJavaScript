379ac99584e21d68a60ea9a88cb7c256d83bd25c
PureDevPer <wooseok.kim1207@gmail.com>
2019-05-03 12:13:05 -0400

    DOM - Document Object Model

--- DOM_Function/index.js
+++ DOM_Function/index.js
@@ -1,5 +1,13 @@
 // DOM = Document Object Model
-const title = document.getElementById("title");
+//const title = document.getElementById("title");
+const title = document.querySelector("#title");
 
 title.innerHTML = "Hi! From JS";
 console.log(title);
+console.dir(title);
+console.dir(document);
+title.style.color = "red";
+document.title = "Document";
+//title.className = "class";
+//console.dir(title);
+
