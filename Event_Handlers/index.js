25a3cbba24b651627db3396e7be606ad2f9460bf
PureDevPer <wooseok.kim1207@gmail.com>
2019-05-15 20:15:04 -0400

    Event Handler

--- Event_Handlers/index.js
+++ Event_Handlers/index.js
@@ -1,4 +1,9 @@
+// https://developer.mozilla.org/en-US/docs/Web/Events
+
+
 const title = document.querySelector('#title');
+const BASE_COLOR = "rgb(52, 73, 94)";
+const OTHER_COLOR = "#7f8c8d";
 
 //function handleResize(event){
     //console.log("I have been resized");
@@ -6,15 +11,25 @@ const title = document.querySelector('#title');
 //}
 
 function handleClick(){
-    if(title.style.color === "blue")
-        title.style.color = "white";
-    else
-        title.style.color = "blue";
+    const currentColor = title.style.color;
+    //console.log(currentColor);
     
+    if(currentColor === BASE_COLOR)
+        title.style.color = OTHER_COLOR;
+    else
+        title.style.color = BASE_COLOR;
+    
+    
+}
+
+function init(){
+    title.style.color = BASE_COLOR;
+    title.addEventListener("click", handleClick);
+    //title.addEventListener("mouseenter", handleClick);
 }
 
 //window.addEventListener("resize", handleResize);
 // Call right now
 //window.addEventListener("resize", handleResize());
 
-title.addEventListener("click", handleClick);
+init();
