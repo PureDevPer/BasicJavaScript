129f3b5e1150a7fba0dc787cef3def639acda6ef
PureDevPer <wooseok.kim1207@gmail.com>
2019-05-20 13:18:43 -0400

    a new page

---
+++ ReactNative/Background, a new page/App.js
@@ -0,0 +1,44 @@
+import React, { Component} from 'react';
+import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import Weather from "./Weather";
+
+export default class App extends Component {
+  state = {
+    isLoaded: true
+  }
+
+  render() {
+    const { isLoaded } = this.state;
+    return (
+      <View style={styles.container}>
+        {isLoaded ? <Weather /> : (
+        <View style={styles.loading}>
+          <Text style={styles.loadingText}>Getting the weather</Text>
+        </View>
+        )}
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    flexDirection: 'row',
+    //justifyContent: 'space-between',
+    //alignItems: 'flex-end',
+    //flexWrap: 'wrap'
+  },
+  loading:{
+    flex: 1,
+    backgroundColor: '#FDF6AA',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-end',
+    paddingLeft: 25
+  },
+  loadingText:{
+    fontSize: 38,
+    marginBottom: 100
+  }
+});
