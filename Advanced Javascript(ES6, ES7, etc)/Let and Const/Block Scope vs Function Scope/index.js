6234d3f3575e7bc7a6b89c65a2ac74cbe5cc4a76
PureDevPer <wooseok.kim1207@gmail.com>
2019-05-31 16:35:24 -0400

    Block scope vs. Function Scope

---
+++ Advanced Javascript(ES6, ES7, etc)/Let and Const/Block Scope vs Function Scope/index.js
@@ -0,0 +1,14 @@
+if(true){
+    var h1 = "hi";
+    let h2 = "hello";
+}
+
+console.log(h1); // hi
+//console.log(h2); // reference error
+
+// function scope
+function a(){
+    var msg = "test";
+}
+
+console.log(msg); // Reference Error
