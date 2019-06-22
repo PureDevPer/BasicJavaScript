516e6f49e940bd722efd4b818d0bafd1f7123b39
PureDevPer <wooseok.kim1207@gmail.com>
2019-06-22 13:29:32 -0400

    Introduction to Async/Await

---
+++ Advanced Javascript(ES6, ES7, etc)/Async and Await/Introduction/index.js
@@ -0,0 +1,17 @@
+const getMoviesPromise = () => {
+	fetch('https://yts.am/api/v2/list_movies.json')
+		.then(response => {
+			console.log(response);
+			return response.json();
+		})
+		.then(json => console.log(json))
+		.catch(e => console.log(`❌ ${e}`));
+};
+
+const getMoviesAsync = async () => {
+	const response = await fetch('https://yts.am/api/v2/list_movies.json');
+	const json = await response.json();
+	console.log(response);
+};
+
+getMoviesAsync();
