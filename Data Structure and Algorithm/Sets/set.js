b297557554574b577394fe64a7af2ed84b3f536a
PureDevPer <wooseok.kim1207@gmail.com>
2019-07-14 16:21:00 -0400

    Set

--- Data Structure and Algorithm/Sets/set.js
+++ Data Structure and Algorithm/Sets/set.js
@@ -61,6 +61,60 @@ class Set {
 		}
 		return values;
 	}
+
+	union(otherSet) {
+		const unionSet = new Set();
+		this.values().forEach(value => unionSet.add(value));
+		otherSet.values().forEach(value => unionSet.add(value));
+		return unionSet;
+	}
+
+	intersection(otherSet) {
+		const intersectionSet = new Set();
+
+		const values = this.values();
+		const otherValues = otherSet.values();
+
+		let biggerSet = values;
+		let smallerSet = otherValues;
+
+		if (otherValues.length - values.length > 0) {
+			biggerSet = otherValues;
+			smallerSet = values;
+		}
+		smallerSet.forEach(value => {
+			if (biggerSet.includes(value)) {
+				intersectionSet.add(value);
+			}
+		});
+
+		return intersectionSet;
+	}
+
+	difference(otherSet) {
+		const differenceSet = new Set();
+		this.values().forEach(value => {
+			if (!otherSet.has(value)) {
+				differenceSet.add(value);
+			}
+		});
+		return differenceSet;
+	}
+
+	isSubsetOf(otherSet) {
+		if (this.size() > otherSet.size()) {
+			return false;
+		}
+		let isSubset = true;
+		this.values().every(value => {
+			if (!otherSet.has(value)) {
+				isSubset = false;
+				return false;
+			}
+			return true;
+		});
+		return isSubset;
+	}
 }
 
 const set = new Set();
@@ -80,3 +134,40 @@ console.log(set.values()); // [2]
 
 set.delete(2);
 console.log(set.values()); // []
+
+const setA = new Set();
+setA.add(1);
+setA.add(2);
+setA.add(3);
+
+const setB = new Set();
+setB.add(3);
+setB.add(4);
+setB.add(5);
+setB.add(6);
+
+const unionAB = setA.union(setB);
+console.log(unionAB.values()); // [1, 2, 3, 4, 5, 6]
+
+const intersectionAB = setA.intersection(setB);
+console.log(intersectionAB.values()); // [3]
+
+const differenceAB = setA.difference(setB);
+console.log(differenceAB.values()); // [1, 2]
+
+const setC = new Set();
+setC.add(1);
+setC.add(2);
+
+const setD = new Set();
+setD.add(1);
+setD.add(2);
+setD.add(2);
+
+const setE = new Set();
+setE.add(2);
+setE.add(3);
+setE.add(4);
+
+console.log(setC.isSubsetOf(setD)); // true
+console.log(setD.isSubsetOf(setE)); // false
