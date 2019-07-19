0048f2b45dc8243d1ee28769ed8e6dea80ce57f2
PureDevPer <wooseok.kim1207@gmail.com>
2019-07-18 20:49:15 -0400

    Merge Sort

--- Data Structure and Algorithm/Sorting Algorithm/mergeSort.js
+++ Data Structure and Algorithm/Sorting Algorithm/mergeSort.js
@@ -1,36 +1,20 @@
-const Compare = {
-	LESS_THAN: -1,
-	BIGGER_THAN: 1,
-	EQUALS: 0
-};
-
-const defaultCompare = (a, b) => {
-	if (a === b) {
-		return Compare.EQUALS;
-	}
-	return a < b ? Compare.LESS_THAN : Compare.BIGGER_THAN;
-};
-
-const merge = (left, right, compareFn) => {
+const merge = (left, right) => {
 	let i = 0;
 	let j = 0;
 	const result = [];
+
 	while (i < left.length && j < right.length) {
-		result.push(
-			compareFn(left[i], right[j]) === Compare.LESS_THAN
-				? left[i++]
-				: right[j++]
-		);
+		result.push(left[i] < right[j] ? left[i++] : right[j++]);
 	}
 	return result.concat(i < left.length ? left.slice(i) : right.slice(j));
 };
-const mergeSort = (array, compareFn = defaultCompare) => {
+const mergeSort = array => {
 	if (array.length > 1) {
 		const { length } = array;
 		const middle = Math.floor(length / 2);
-		const left = mergeSort(array.slice(0, middle), compareFn);
-		const right = mergeSort(array.slice(middle, length), compareFn);
-		array = merge(left, right, compareFn);
+		const left = mergeSort(array.slice(0, middle));
+		const right = mergeSort(array.slice(middle, length));
+		array = merge(left, right);
 	}
 	return array;
 };
