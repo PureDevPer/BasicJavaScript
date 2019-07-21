1e5857afd058b6cb62af6c3b666cbabf5daed9e5
PureDevPer <wooseok.kim1207@gmail.com>
2019-07-21 15:13:35 -0400

    Knapsack

--- Data Structure and Algorithm/Algorithm Designs and Techniques/Dynamic Programming/knapsack.js
+++ Data Structure and Algorithm/Algorithm Designs and Techniques/Dynamic Programming/knapsack.js
@@ -1,4 +1,23 @@
-const knapsack = (capacity, weights, values, n) => {
+const findValues = (n, capacity, kS, weights, values) => {
+	let i = n; // 3
+	let k = capacity; // 5
+	console.log('Items that are part of the solution:');
+	while (i > 0 && k > 0) {
+		if (kS[i][k] !== kS[i - 1][k]) {
+			console.log(
+				`item ${i} can be part of solution w,v: ${weights[i - 1]}, ${
+					values[i - 1]
+				}`
+			);
+			--i;
+			k -= kS[i][k];
+		} else {
+			--i;
+		}
+	}
+};
+
+const knapSack = (capacity, weights, values, n) => {
 	const kS = [];
 	for (let i = 0; i <= n; ++i) {
 		kS[i] = [];
@@ -6,9 +25,12 @@ const knapsack = (capacity, weights, values, n) => {
 
 	for (let i = 0; i <= n; ++i) {
 		for (let w = 0; w <= capacity; ++w) {
+			// Ignore first column and row of the matrix
 			if (i == 0 || w == 0) {
 				kS[i][w] = 0;
 			} else if (weights[i - 1] <= w) {
+				// Item i can only be part of the solution if its weight is less than the capacity
+
 				const a = values[i - 1] + kS[i - 1][w - weights[i - 1]];
 				const b = kS[i - 1][w];
 				kS[i][w] = a > b ? a : b;
@@ -21,3 +43,11 @@ const knapsack = (capacity, weights, values, n) => {
 	findValues(n, capacity, kS, weights, values);
 	return kS[n][capacity];
 };
+
+const values = [3, 4, 5];
+const weights = [2, 3, 4];
+const capacity = 5;
+const n = values.length;
+console.log(
+	`Total value that can be carried: ${knapSack(capacity, weights, values, n)}`
+);
